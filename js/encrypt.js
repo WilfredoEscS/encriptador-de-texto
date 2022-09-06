63cf63ac1cb6164e1297be69b4f779ef5b8308e3
Wilfredo Escobar <wilfredosorto75@gmail.com>
2022-09-06 11:41:28 -0600

    Feat: Add the function to decrypt the text and display it
    * Create the function and bind it to the decrypt button
    
    * Shows the decrypted text on the screen

--- js/encrypt.js
+++ js/encrypt.js
@@ -2,11 +2,11 @@ const imputText = document.querySelector(".data-input__textarea");
 const message = document.querySelector(".encryptor-result__message");
 const image = document.querySelector(".encryptor-result__image");
 const info = document.querySelector(".encryptor-result__info");
-const copyBttn = document.querySelector(".copy-button")
+const copyBtn = document.querySelector(".copy-button")
 
 message.value = ""
 function encrypt(encryptedText) {
-    let encryptionKeys = [["a","ai"], ["e","enter"], ["i","imes"], ["o","ober"], ["u","ufat"]]
+    let encryptionKeys = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]]
     encryptedText = encryptedText.toLowerCase();
     for (let i = 0; i < encryptionKeys.length; i++) {
         if (encryptedText.includes(encryptedText[i][0])) {
@@ -15,7 +15,6 @@ function encrypt(encryptedText) {
     }
     return encryptedText
 }
-
 function encryptBtn() {
     const encryptMsg = encrypt(imputText.value);
     message.value = encryptMsg;
@@ -23,5 +22,27 @@ function encryptBtn() {
     imputText.value = "";
     image.style.display = "none"
     info.style.display = "none"
-    copyBttn.style.display = "flex" 
+    copyBtn.style.display = "flex" 
+}
+
+/* Funcion para desencriptar texto */
+
+function decrypt(decryptedText){
+    let decryptionKeys = [["enter","e"], ["imes","i"], ["ai","a"], ["ober","o"], ["ufat","u"]]
+    decryptedText = decryptedText.toLowerCase();
+    for (let i = 0; i < decryptionKeys.length; i++) {
+        if (decryptedText.includes(decryptedText[i][0])) {
+            decryptedText = decryptedText.replaceAll(decryptionKeys[i][0], decryptionKeys[i][1]) 
+        } 
+    }
+    return decryptedText
+}
+function decryptBtn() {
+    const decryptMsg = decrypt(imputText.value);
+    message.value = decryptMsg;
+    message.setAttribute('rows','8')
+    imputText.value = "";
+    image.style.display = "none"
+    info.style.display = "none"
+    copyBtn.style.display = "flex" 
 }
